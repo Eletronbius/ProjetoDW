39dbcd912a6b7ab06c595f5722ccf15ea42723c3
Eletronbius <121073889+Eletronbius@users.noreply.github.com>
2023-10-11 12:49:01 -0300

    Criado

---
+++ js/getProd.js
@@ -0,0 +1,29 @@
+function getProd() {
+    const userId = document.getElementById("getProdId").value;
+
+    fetch('/backend/produtos.php?id=' + userId, {
+        method: 'GET'
+    })
+    .then(response => {
+        if (!response.ok) {
+            if (response.status === 401) {
+                throw new Error('Não autorizado');
+            } else {
+                throw new Error('Sem rede ou não conseguiu localizar o recurso');
+            }
+        }
+        return response.json();
+    })
+    .then(data => {
+        if(!data.status){
+            alert('Produto não encontrado')
+            document.getElementById("inpuNome").value = ''; 
+        }else{
+            document.getElementById("inpuNome").value = data.usuario.nome; 
+            document.getElementById("inpuPreco").value = data.usuario.preco; 
+            document.getElementById("inpuQuantidade").value = data.usuario.quantidade; 
+        } 
+       
+    })
+    .catch(error => alert('Erro na requisição: ' + error));
+}
